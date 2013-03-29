6a069481d96b5a5d467ea08e4f909f77f89a47e8
Victor Valotto <vvalotto@gmail.com>
2013-03-29 12:05:57 -0300

    Alcance

---
+++ Inicio /Alcance.py
@@ -0,0 +1,25 @@
+'''
+Created on 28/03/2013
+
+@author: voval
+'''
+def scope_test():
+    def do_local():
+        spam= "local spam"
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+    def do_global():
+        global spam
+        spam = "global spam"
+        
+    spam = "test_spam"
+    do_local()
+    print("Despues del local:", spam)
+    do_nonlocal()
+    print('Despues del nonlocal:', spam)
+    do_global()
+    print('Despues del global:', spam)
+    
+scope_test()
+print('Ambito global:', spam)
---
+++ Inicio /Variables.py
@@ -0,0 +1,7 @@
+'''
+Created on 28/03/2013
+
+@author: voval
+'''
+#!/usr/bin/env python
+
