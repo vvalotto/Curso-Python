a07e8c764d775649843815dd7f4b6b0fd31f4a66
unknown <Victor@.(none)>
2013-06-06 17:45:15 -0300

    Arreglo a p3

--- 3 - Internet Client Programming/getLatestFTP.py
+++ 3 - Internet Client Programming/getLatestFTP.py
@@ -11,36 +11,36 @@ FILE = 'bugzilla-LATEST.tar.gz'
 def main():
     try:
         f = ftplib.FTP(HOST)
-    except (socket.error, socket.gaierror), e:
-        print 'ERROR: cannot reach "%s"' % HOST
+    except (socket.error, socket.gaierror) as e:
+        print ('ERROR: cannot reach "%s"' % HOST)
         return
-    print '*** Connected to host "%s"' % HOST
+    print ('*** Connected to host "%s"' % HOST)
 
     try:
         f.login()
     except ftplib.error_perm:
-        print 'ERROR: cannot login anonymously'
+        print ('ERROR: cannot login anonymously')
         f.quit()
         return
-    print '*** Logged in as "anonymous"'
+    print ('*** Logged in as "anonymous"')
 
     try:
         f.cwd(DIRN)
     except ftplib.error_perm:
-        print 'ERROR: cannot CD to "%s" folder' % DIRN
+        print ('ERROR: cannot CD to "%s" folder' % DIRN)
         f.quit()
         return
-    print '*** Changed to "%s" folder' % DIRN
+    print ('*** Changed to "%s" folder' % DIRN)
 
     try:
         f.retrbinary('RETR %s' % FILE,
             open(FILE, 'wb').write)
     except ftplib.error_perm:
-        print 'ERROR: cannot read file "%s"' % FILE
+        print ('ERROR: cannot read file "%s"' % FILE)
         if os.path.exists(FILE):
             os.unlink(FILE)
     else:
-        print '*** Downloaded "%s" to CWD' % FILE
+        print ('*** Downloaded "%s" to CWD' % FILE)
     f.quit()
 
 if __name__ == '__main__':
--- Inicio /Variables.py
+++
@@ -1,7 +0,0 @@
-'''
-Created on 28/03/2013
-
-@author: voval
-'''
-#!/usr/bin/env python
-
