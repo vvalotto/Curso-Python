c898dda50d4ba1221e5d6fe6bb7e46dfd11dd8fc
Victor Valotto <vvalotto@gmail.co>
2015-08-15 10:07:23 -0300

    correcciones menores

--- _01_Introduccion/04_Archivos/escribir_archivo.py
+++ _01_Introduccion/04_Archivos/escribir_archivo.py
@@ -7,6 +7,6 @@ f = open("salida","w")
 while anio <= numanios:
     principal = principal * (1 + tasa)
     #salida formateada mejorada 3
-    print("{0:3d} {1:0.2f}".format(anio,principal), file=f)
+    print("{0:3d} {1:0.2f}".format(anio, principal), file=f)
     anio += 1
 f.close()
